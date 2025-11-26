a8009ec2df6540391395b5978fcabe1a930adea0
Dibya Ranjan Sahu <dibyax.9@gmail.com>
2025-11-26 18:11:49 +0530

    Added new routes based on controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,6 +4,13 @@ import {
   logoutUser,
   registerUser,
   refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateAvatar,
+  updateCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
@@ -32,4 +39,21 @@ router.route("/logout").post(verifyJwt, logoutUser);
 
 router.route("/refresh-token").post(refreshAccessToken);
 
+router.route("/change-password").post(verifyJwt, changeCurrentPassword);
+
+router.route("/current-user").get(verifyJwt, getCurrentUser);
+
+router.route("update-account").patch(verifyJwt, updateAccountDetails);
+
+router
+  .route("/update-avatar")
+  .patch(verifyJwt, upload.single("avatar"), updateAvatar);
+
+router
+  .route("/cover-image")
+  .patch(verifyJwt, upload.single("/coverImage"), updateCoverImage);
+
+router.route("/c/:userName").get(verifyJwt, getUserChannelProfile);
+
+router.route("watch-history").get(verifyJwt, getWatchHistory);
 export default router;
